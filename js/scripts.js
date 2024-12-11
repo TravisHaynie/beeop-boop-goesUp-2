dbc5dda779a04922ff328ea5ae75892ca1253033
unknown <travihay@gmail.com>
2024-12-11 17:03:11 -0600

    added code

--- js/scripts.js
+++ js/scripts.js
@@ -5,28 +5,39 @@ let guestEl = document.getElementById("guest");
 let periodEl = document.getElementById("period");
 let guestFoulesEl = document.getElementById("guest-fouls");
 let homeFoulsEl = document.getElementById("home-fouls");
-let buttonEl = document.getElementById(".btn");
+let buttonEl = document.querySelectorAll(".btn");
+let startButtonEl = document.getElementById("startgame")
+console.log(buttonEl)
 let countHome = 0;
 let countGuest = 0;
 let countHfouls = 0;
 let countGfouls = 0
 let countPeriod= 0;
 
+
 function homeOneCounter() {
     countHome += 1
     homeEl.textContent = countHome
+    
 
     if(countHome >= 10) {
         homeEl.style.border = "2px solid red"
         leaderEl.textContent = "HOME WINS"
+        // when using setAttribute it only seem to work on the first function that had to attached to it. Using querSelectorAll caused an error in the console saying setAttribute is not a function. Could not get past this issue.
+        buttonEl.forEach(button => {
+            button.disabled = true;
+          });
+        console.log(buttonEl)
     } else if(countHome <= 10 && countHome > countGuest){
         leaderEl.textContent = "Home is in the lead"
     } else if(countGuest === countHome){
         leaderEl.textContent = "TIE"
     } else (
-        leaderEl.textContent = "blah"
+          leaderEl.textContent = "Guest is in the lead"
     )
 }
+
+
  
 function homeTwoCounter() {
     countHome += 2
@@ -36,14 +47,20 @@ function homeTwoCounter() {
     if(countHome >= 10) {
         homeEl.style.border = "2px solid red"
         leaderEl.textContent = "HOME WINS"
+        buttonEl.forEach(button => {
+            button.disabled = true;
+          });
+        console.log(buttonEl)
     } else if(countHome <= 10 && countHome > countGuest){
         leaderEl.textContent = "Home is in the lead"
     } else if(countGuest === countHome){
         leaderEl.textContent = "TIE"
     } else (
-        leaderEl.textContent = "blah"
+         leaderEl.textContent = "Guest is in the lead"
     )
 }
+
+
 
 function homeThreeCounter() {
     countHome += 3
@@ -53,12 +70,16 @@ function homeThreeCounter() {
     if(countHome >= 10) {
         homeEl.style.border = "2px solid red"
         leaderEl.textContent = "HOME WINS"
+        buttonEl.forEach(button => {
+            button.disabled = true;
+          });
+        console.log(buttonEl)
     } else if(countHome <= 10 && countHome > countGuest){
         leaderEl.textContent = "Home is in the lead"
     } else if(countGuest === countHome){
         leaderEl.textContent = "TIE"
     } else (
-        leaderEl.textContent = "blah"
+        leaderEl.textContent = "Guest is in the lead"
     )
 }
 
@@ -70,12 +91,16 @@ function guestOneCounter() {
     if(countGuest >= 10) {
         guestEl.style.border = "2px solid red"
         leaderEl.textContent = "GUEST WINS BOO"
+        buttonEl.forEach(button => {
+            button.disabled = true;
+          });
+        console.log(buttonEl)
     } else if(countGuest <= 10 && countGuest > countHome){
         leaderEl.textContent = "Guest is in the lead"
     } else if(countGuest === countHome){
         leaderEl.textContent = "TIE"
     } else (
-        leaderEl.textContent = "blah"
+         leaderEl.textContent = "Home is in the lead"
     )
 }
  
@@ -87,12 +112,16 @@ function guestTwoCounter() {
     if(countGuest >= 10) {
         guestEl.style.border = "2px solid red"
         leaderEl.textContent = "GUEST WINS BOO"
+        buttonEl.forEach(button => {
+            button.disabled = true;
+          });
+        console.log(buttonEl)
     } else if(countGuest <= 10 && countGuest > countHome){
         leaderEl.textContent = "Guest is in the lead"
     } else if(countGuest === countHome){
         leaderEl.textContent = "TIE"
     } else (
-        leaderEl.textContent = "blah"
+      leaderEl.textContent = "Home is in the lead"
     )
 }
 
@@ -104,12 +133,16 @@ function guestThreeCounter() {
     if(countGuest >= 10) {
         guestEl.style.border = "2px solid red"
         leaderEl.textContent = "GUEST WINS BOO"
+        buttonEl.forEach(button => {
+            button.disabled = true;
+          });
+        console.log(buttonEl)
     } else if(countGuest <= 10 && countGuest > countHome){
         leaderEl.textContent = "Guest is in the lead"
     } else if(countGuest === countHome){
         leaderEl.textContent = "TIE"
     } else (
-        leaderEl.textContent = "blah"
+          leaderEl.textContent = "Home is in the lead"
     )
 }
 
@@ -129,15 +162,39 @@ function guestFouls() {
 
 }
 
-function gameLeader() {
+function startGame() {
+    let gameTimeEl = gameTimer();
 
+    if(startGame) {
+        gameTimeEl
+        startButtonEl.setAttribute('disabled',"")
+    } else {
+       
+    }
+    
 }
 
 function newGame() {
+ let newGameGenerator = [guestOneCounter, guestTwoCounter, guestThreeCounter, homeOneCounter, homeTwoCounter, homeThreeCounter]
 
+ for (let i = 0; i < newGameGenerator.length; i++) {
+    console.log(newGameGenerator)
+        if(newGameGenerator) {
+         homeEl.textContent = 0
+         guestEl.textContent = 0
+         periodEl.textContent = 0
+         homeFoulsEl.textContent = 0
+         guestFoulesEl.textContent = 0
+         gameTimer = location.reload() // how do you bottom out a timer by only using the state?
+         buttonEl.forEach(button => {
+         button.disabled = false;
+          });
+        }
+    
+ }
 }
 
-function gameTimer() {
+const gameTimer = function() {
     let timerName = "Timer:"
     let time = 30;
     setInterval(()=> {
@@ -146,11 +203,10 @@ function gameTimer() {
             time -- 
             timerEl.textContent = timerName + " " + time
             console.log(time)
-        } else if(time <= 0) {
+        }  else if(time <= 0) {
             console.log(time)
             timerEl.textContent = "Game Over"
         }
     },1000)
 }
 
-gameTimer()
